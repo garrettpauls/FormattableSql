0d6e1b589cfd1e4b881be08f86ae2ffbbf4a3fa8
Garrett Pauls <garrett.pauls@gmail.com>
2015-12-13 16:09:33 -0700

    Changed nuget output directory

--- build-nuget.bat
+++ build-nuget.bat
@@ -1,5 +1,5 @@
 set projFile=%~dp0\Source\FormattableSql.Core\FormattableSql.Core.csproj
-set outputDir=%~dp0\Build
+set outputDir=%~dp0\Package
 set msbuild=C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe
 
 "%msbuild%" "%projFile%" /p:Configuration=Release
