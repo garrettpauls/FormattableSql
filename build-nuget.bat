e54cdf9c65e92282382d76eef2643a1f836f2695
Garrett Pauls <garrett.pauls@gmail.com>
2015-12-13 16:05:12 -0700

    Added nuget packaging script

---
+++ build-nuget.bat
@@ -0,0 +1,7 @@
+set projFile=%~dp0\Source\FormattableSql.Core\FormattableSql.Core.csproj
+set outputDir=%~dp0\Build
+set msbuild=C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe
+
+"%msbuild%" "%projFile%" /p:Configuration=Release
+mkdir "%outputDir%"
+"%~dp0\.nuget\nuget.exe" pack "%projFile%" -Prop Configuration=Release -OutputDirectory "%outputDir%"
